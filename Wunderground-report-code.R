982f8c331b2fc8539934ca9913bc07362fca335e
joshtharvey <josh.harvey@agnet.tamu.edu>
2021-06-11 15:58:19 -0500

    making progress on animation

--- Wunderground-report-code.R
+++ Wunderground-report-code.R
@@ -178,14 +178,6 @@ test<-as.data.frame(`hourly_table.2021-06-07`[[1]])
 test<-get(paste0("hourly_table.",weeks_dates[3]), envir = .GlobalEnv)
 test<-as.data.frame(test[[1]])
 
-dput(test)
-
-
-
-
-
-
-
 plot(test[2,], xlim=c(0,50))
 plot.ts(test[c(1,2),])
 tempts <- ts(test[,c(1,2)])
@@ -205,9 +197,106 @@ tst[,c(2:4,6:9)]<-lapply(tst[,c(2:4,6:9)], as.numeric)
 
 plot(tst$Time, tst$Temperature, type='l',las=1, ylim=c(50,120))
 
-
-
 rD$server$stop() #Close RSelenium server
 system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE) #Force quit Java processes
 
-#
+# //////////////////////////////  TEST PLOTTING ///////////////////////////////////////////////
+
+
+test<-structure(list(Time = c("12:15 AM", "12:35 AM", "12:55 AM", "1:15 AM", 
+"1:35 AM", "1:55 AM", "2:15 AM", "2:35 AM", "2:55 AM", "3:15 AM", 
+"3:35 AM", "3:55 AM", "4:15 AM", "4:35 AM", "4:55 AM", "5:15 AM", 
+"5:35 AM", "5:55 AM", "6:15 AM", "6:35 AM", "6:55 AM", "7:15 AM", 
+"7:35 AM", "7:55 AM", "8:15 AM", "8:35 AM", "8:55 AM", "9:15 AM", 
+"9:35 AM", "9:55 AM", "10:15 AM", "10:35 AM", "10:55 AM", "11:15 AM", 
+"11:35 AM", "11:55 AM", "12:15 PM", "12:35 PM", "12:55 PM"), 
+Temperature = c("79 F", "79 F", "79 F", "78 F", "78 F", "77 F", 
+"77 F", "76 F", "76 F", "76 F", "76 F", "75 F", "75 F", "75 F", 
+"75 F", "74 F", "74 F", "74 F", "74 F", "74 F", "74 F", "74 F", 
+"74 F", "74 F", "75 F", "75 F", "76 F", "76 F", "78 F", "78 F", 
+"79 F", "79 F", "80 F", "80 F", "83 F", "83 F", "84 F", "84 F", 
+"85 F"), `Dew Point` = c("79 F", "79 F", "79 F", "78 F", 
+"78 F", "77 F", "77 F", "76 F", "76 F", "76 F", "76 F", "75 F", 
+"75 F", "75 F", "75 F", "74 F", "74 F", "74 F", "74 F", "74 F", 
+"74 F", "74 F", "74 F", "74 F", "75 F", "75 F", "76 F", "76 F", 
+"75 F", "76 F", "76 F", "76 F", "76 F", "76 F", "76 F", "76 F", 
+"75 F", "75 F", "75 F"), Humidity = c("100 %", "100 %", "100 %", 
+"100 %", "100 %", "100 %", "100 %", "100 %", "100 %", "100 %", 
+"100 %", "100 %", "100 %", "100 %", "100 %", "100 %", "100 %", 
+"100 %", "100 %", "100 %", "100 %", "100 %", "100 %", "100 %", 
+"100 %", "100 %", "100 %", "99 %", "92 %", "94 %", "91 %", 
+"90 %", "88 %", "87 %", "81 %", "79 %", "74 %", "73 %", "72 %"
+), Wind = c("SE", "SE", "SE", "SE", "SE", "SE", "SE", "ESE", 
+"ESE", "ESE", "SE", "SE", "ESE", "SE", "ESE", "ESE", "ESE", 
+"ESE", "ESE", "ESE", "SE", "SE", "SE", "ESE", "SE", "SE", 
+"SSE", "SSE", "SSE", "SE", "SSE", "SSE", "SSE", "S", "SSE", 
+"SSE", "SSE", "S", "SSE"), `Wind Speed` = c("9 mph", "10 mph", 
+"10 mph", "7 mph", "7 mph", "8 mph", "9 mph", "6 mph", "7 mph", 
+"8 mph", "7 mph", "7 mph", "9 mph", "9 mph", "6 mph", "7 mph", 
+"7 mph", "8 mph", "6 mph", "9 mph", "9 mph", "7 mph", "5 mph", 
+"8 mph", "8 mph", "8 mph", "9 mph", "10 mph", "9 mph", "9 mph", 
+"10 mph", "9 mph", "9 mph", "7 mph", "8 mph", "8 mph", "7 mph", 
+"9 mph", "8 mph"), `Wind Gust` = c("0 mph", "16 mph", "16 mph", 
+"16 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", 
+"0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", 
+"0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", 
+"0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", 
+"0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", "0 mph", 
+"0 mph"), Pressure = c("28.92 in", "28.91 in", "28.91 in", 
+"28.91 in", "28.90 in", "28.90 in", "28.90 in", "28.90 in", 
+"28.89 in", "28.90 in", "28.89 in", "28.89 in", "28.89 in", 
+"28.89 in", "28.89 in", "28.89 in", "28.89 in", "28.89 in", 
+"28.89 in", "28.89 in", "28.89 in", "28.90 in", "28.90 in", 
+"28.91 in", "28.91 in", "28.91 in", "28.92 in", "28.92 in", 
+"28.92 in", "28.92 in", "28.92 in", "28.92 in", "28.92 in", 
+"28.93 in", "28.92 in", "28.93 in", "28.93 in", "28.93 in", 
+"28.93 in"), Precip. = c("0.0 in", "0.0 in", "0.0 in", "0.0 in", 
+"0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", 
+"0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", 
+"0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", 
+"0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", 
+"0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in", 
+"0.0 in", "0.0 in", "0.0 in", "0.0 in", "0.0 in"), Condition = c("Mostly Cloudy", 
+"Cloudy", "Cloudy", "Cloudy", "Cloudy", "Mostly Cloudy", 
+"Mostly Cloudy", "Mostly Cloudy", "Cloudy", "Cloudy", "Cloudy", 
+"Cloudy", "Cloudy", "Cloudy", "Cloudy", "Mostly Cloudy", 
+"Mostly Cloudy", "Cloudy", "Cloudy", "Cloudy", "Cloudy", 
+"Cloudy", "Cloudy", "Cloudy", "Cloudy", "Cloudy", "Cloudy", 
+"Cloudy", "Cloudy", "Cloudy", "Cloudy", "Cloudy", "Cloudy", 
+"Cloudy", "Cloudy", "Cloudy", "Cloudy", "Cloudy", "Mostly Cloudy"
+)), row.names = c(NA, -39L), class = "data.frame")
+
+
+test
+
+test$Time <- strptime(test$Time, '%I:%M %p')
+
+test[,c(2:4,6:9)]<-apply(test[,c(2:4,6:9)], MARGIN = 2, function(x) gsub("[A-z]+|[:%:]","",x))
+test[,c(2:4,6:9)]<-lapply(test[,c(2:4,6:9)], as.numeric)
+
+plot(test$Time, test$Temperature, type='l',las=1)
+hops<-as.data.frame(approx(test$Time, test$Temperature, method='linear', n=200))
+hops2<-as.data.frame(approx(test$Time, test$Humidity, method='linear', n=200))
+
+
+require(scales)
+points(hops[1,], col=alpha('red', 0.4), pch=16)
+
+
+
+library(animation)
+
+
+
+saveGIF({
+ani.options(nmax=200)
+for(i in 1:ani.options('nmax')){
+  plot(test$Time, test$Temperature, type='l',las=1, ylim=c(70,100))
+  lines(test$Time, test$Humidity, col='lightblue')
+  points(hops[i,], col=alpha('red', 0.4), pch=16)
+  points(hops2[i,], col=alpha('blue', 0.4), pch=16)
+}
+}, interval = 0.01, movie.name ='anitest.gif')
+
+100%%.005
+
